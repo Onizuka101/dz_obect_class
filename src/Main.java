7d5089a73439e2a54f0e91d2008e404ee5ca9849
Onizuka101 <mudrik898710@yandex.ru>
2023-02-10 19:38:55 +0300

    23

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
-        book book1 = new book("book1", 1990);
-        System.out.println("book1.name = " + book1.getName());
-        System.out.println("book1.age = " + book1.getAge());
-        book1.setAge(1999);
-        System.out.println("book1.getAge() = " + book1.getAge());
-        Authur authur = new  Authur("Ivan", "Ivanov");
-        System.out.println("authur.nameAuthur = " + authur.getNameAuthur());
-        System.out.println("authur.surnameAuthur = " + authur.getSurnameAuthur());
-        book book2 = new book("book1", 1899 );
-        System.out.println("book2.getName() = " + book2.getName());
-        System.out.println("book2.getAge() = " + book2.getAge());
+        Authur authur1 = new  Authur("Ivan", "Ivanov");
         Authur authur2 = new Authur("Petr", "Petrov");
-        System.out.println("authur2.getNameAuthur() = " + authur2.getNameAuthur());
-        System.out.println("authur2.getSurnameAuthur() = " + authur2.getSurnameAuthur());
+        book book1 = new book("book1", 1990, authur1);
+        book book2 = new book("book2", 1899, authur2 );
+        System.out.println(book1);
+        System.out.println(book2);
+
 
 
     }
--- src/book.java
+++ src/book.java
@@ -1,9 +1,14 @@
 public class book {
-  private   String name;
-  private   int age;
-    public book (String name, int age){
+    private   String name;
+    private   int age;
+    private Authur authur;
+
+
+    public book (String name, int age,Authur authur){
         this.name=name;
         this.age=age;
+        this.authur=authur;
+
     }
 
     public String getName() {
@@ -14,5 +19,12 @@ public class book {
     }
     public void setAge (int age) {
         this.age=age;
+    }
+    public Authur getAuthur() {
+        return this.authur;
+    }
+
+    public String toString() {
+        return "имя " + this.name + " год " + this.age + " ИФ автора " + this.authur;
     }
 }
