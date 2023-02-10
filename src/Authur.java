df43c9ad58ef4b2c11a7cfb7e857ab4b003204a5
Onizuka101 <mudrik898710@yandex.ru>
2023-02-10 17:31:07 +0300

    12

---
+++ src/Authur.java
@@ -0,0 +1,15 @@
+public class Authur {
+    private String nameAuthur;
+    private String surnameAuthur;
+
+    public Authur (String nameAuthur, String surnameAuthur) {
+        this.nameAuthur= nameAuthur;
+        this.surnameAuthur=surnameAuthur;
+    }
+    public String getNameAuthur() {
+        return this.nameAuthur;
+    }
+    public String getSurnameAuthur() {
+        return this.surnameAuthur;
+    }
+}
